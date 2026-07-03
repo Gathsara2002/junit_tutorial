ea6cc38d3dfc05a13549899032aeaa30445e93f9
Gathsara <liviru2002mmg@gmail.com>
2026-07-03 13:30:09 +0530

    write test cases

---
+++ src/test/java/com/junit/CalcTest.java
@@ -0,0 +1,32 @@
+package com.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalcTest {
+
+    @Test
+    void add() {
+        Calc calc = new Calc();
+        assertEquals(30, calc.add(10, 20));
+    }
+
+    @Test
+    void sub() {
+        Calc calc = new Calc();
+        assertEquals(5, calc.sub(10, 5));
+    }
+
+    @Test
+    void mul() {
+        Calc calc = new Calc();
+        assertEquals(200, calc.mul(10, 20));
+    }
+
+    @Test
+    void div() {
+        Calc calc = new Calc();
+        assertEquals(2, calc.div(10, 5));
+    }
+}
