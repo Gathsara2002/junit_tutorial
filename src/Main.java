1f5ac0674ea669bf73c0723ac0ad04ebc470a231
Gathsara <liviru2002mmg@gmail.com>
2026-07-03 11:11:56 +0530

    testing manually without junit'

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Calc calc = new Calc();
+
+        int divide = calc.divide(4, 2);
+        System.out.println(divide);
+
+        if (divide == 2){
+            System.out.println("test case 1 is passed");
+        }else {
+            System.out.println("test case 1 is failed");
+        }
+
+        int result = calc.divide(15, 3);
+        if (result == 5){
+            System.out.println("test case 2 is passed");
+        }else {
+            System.out.println("test case 2 is failed");
+        }
+
     }
 }
