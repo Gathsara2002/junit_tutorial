0700ea2bfe202d625acb4bfdaed3a1cd3e3526f4
Gathsara <liviru2002mmg@gmail.com>
2026-07-05 10:39:11 +0530

    Test driven architecture approach

---
+++ src/main/java/com/junit/test_driven_architecture/Shape.java
@@ -0,0 +1,12 @@
+package com.junit.test_driven_architecture;
+
+public class Shape {
+
+    public double areaOfSquare(double length) {
+        return length * length;
+    }
+
+    public double areaOfCircle(double radius) {
+        return Math.round(Math.PI * radius * radius);
+    }
+}
---
+++ src/test/java/com/junit/test_driven_architecture/ShapeTest.java
@@ -0,0 +1,20 @@
+package com.junit.test_driven_architecture;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapeTest {
+
+    Shape shape = new Shape();
+
+    @Test
+    void areaOfSquare() {
+        assertEquals(100, shape.areaOfSquare(10));
+    }
+
+    @Test
+    void areaOfCircle() {
+        assertEquals(154, shape.areaOfCircle(7), "Test failed");
+    }
+}
