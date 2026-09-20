c0535f55dd48e9bd6931a464868a3fd31349d417
Gathsara <liviru2002mmg@gmail.com>
2026-09-20 17:43:50 +0530

    parameterized test

--- src/test/java/com/learning/lifecycle/BankAccountTest.java
+++ src/test/java/com/learning/lifecycle/BankAccountTest.java
@@ -1,6 +1,13 @@
 package com.learning.lifecycle;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -48,5 +55,51 @@ class BankAccountTest {
     @AfterAll
     static void tearDownAll() {
         System.out.println("After All");
+    }
+
+
+    //parameterized tests
+    @ParameterizedTest
+    @ValueSource(ints = {100, 200, 500})
+    void shouldDepositMoney(int amount) {
+
+        account.deposit(amount);
+
+        assertEquals(amount, account.getBalance());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "100, 100",
+            "200, 200",
+            "500, 500"
+    })
+    void shouldDepositMoney(int amount, int expectedBalance) {
+
+        account.deposit(amount);
+
+        assertEquals(expectedBalance, account.getBalance());
+    }
+
+    @ParameterizedTest
+    @MethodSource("depositTestData")
+    void shouldCalculateBalance(
+            int startingBalance,
+            int depositAmount,
+            int expectedBalance) {
+
+        account.deposit(startingBalance);
+        account.deposit(depositAmount);
+
+        assertEquals(expectedBalance, account.getBalance());
+    }
+
+    static Stream<Arguments> depositTestData() {
+
+        return Stream.of(
+                Arguments.of(100, 50, 150),
+                Arguments.of(200, 100, 300),
+                Arguments.of(500, 250, 750)
+        );
     }
 }
